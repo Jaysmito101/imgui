d42edfa7a274061b89a0a96b8257b4130579757c
Jaysmito Mukherjee <73700725+Jaysmito101@users.noreply.github.com>
2021-11-11 19:34:47 +0530

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,18 +1,21 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+	staticruntime "on"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin/intermediates/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
+		"backends/imgui_impl_glfw.cpp",
+		"backends/imgui_impl_opengl3.cpp",
 		"imconfig.h",
 		"imgui.h",
 		"imgui.cpp",
 		"imgui_draw.cpp",
 		"imgui_internal.h",
+		"imgui_tables.cpp",
 		"imgui_widgets.cpp",
 		"imstb_rectpack.h",
 		"imstb_textedit.h",
